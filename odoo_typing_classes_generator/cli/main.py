2cbc72d7ab833b2bfb96227dc2d965595ed5fbd9
Alexandre Galdeano <alexandre.galdeano@foodles.co>
2025-09-18 19:02:35 +0200

    [FIX] CLI: removed unnecessary click-odoo dependency
    Also:
    * Types can be generated on multiple modules
    * Added argument description

--- odoo_typing_classes_generator/cli/main.py
+++ odoo_typing_classes_generator/cli/main.py
@@ -1,21 +1,33 @@
 import logging
 
 import click
-import click_odoo
 
 from odoo_typing_classes_generator.core.generator import Generator
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 _logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.argument("module", required=True)
-@click.argument("addons_path", required=True)
-@click_odoo.env_options()
-def main(env, module: str, addons_path: str):
-    _logger.info(f"[{module}] Generating typing classes...")
-    Generator(addons_path).generate(module)
-    _logger.info(f"[{module}] Done generating typing classes.")
+@click.option(
+    "--modules",
+    help="Comma-separated list of Odoo modules to generate typing classes for",
+    required=True,
+)
+@click.option(
+    "--addons-path",
+    required=True,
+    help="Path where the modules are location, relative to the current working directory",
+)
+def main(modules: str, addons_path: str):
+    for module in modules.split(","):
+        _logger.info(f"[{module}] Generating typing classes...")
+        Generator(addons_path).generate(module)
+        _logger.info(f"[{module}] Done generating typing classes.")
 
 
 if __name__ == "__main__":
